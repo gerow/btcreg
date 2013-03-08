c269abaf5c5549aa066f5d4b17d33dd249e6f7fd
gerow <gerow.mike@gmail.com>
2013-03-07 21:02:41 -0800

    Created basic router config

--- btcreg.go
+++ btcreg.go
@@ -1,4 +1,15 @@
 package btcreg
 
+import (
+    "net/http"
+    "github.com/gorilla/mux"
+)
+
 func Main() {
+    r := mux.NewRouter()
+    r.HandleFunc("/", HomeHandler).Methods("GET")
+    r.HandleFunc("/q/{email}", QueryHandler).Methods("GET")
+    r.HandleFunc("/n/{uuid}", NewAddressFormHandler).Methods("GET")
+    r.HandleFunc("/n/{uuid}", NewAddressHandler).Methods("POST")
+    http.ListenAndServe(":8080", r)
 }
---
+++ controllers.go
@@ -0,0 +1,17 @@
+package btcreg
+
+import (
+    "net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+}
+
+func QueryHandler(w http.ResponseWriter, req *http.Request) {
+}
+
+func NewAddressFormHandler(w http.ResponseWriter, req *http.Request) {
+}
+
+func NewAddressHandler(w http.ResponseWriter, req *http.Request) {
+}
