75f53ec2603f4115b7e37f41ca92d9d3785ea9d2
gerow <gerow.mike@gmail.com>
2013-03-07 21:31:27 -0800

    Added debugging info to controllers

--- controllers.go
+++ controllers.go
@@ -2,6 +2,7 @@ package btcreg
 
 import (
     "net/http"
+    "github.com/gorilla/mux"
     "fmt"
 )
 
@@ -10,21 +11,31 @@ func HomeHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 func QueryHandler(w http.ResponseWriter, req *http.Request) {
+    vars := mux.Vars(req)
     fmt.Println("Query handler called!")
+    fmt.Println("Got email " + vars["email"])
 }
 
 func JsonQueryHandler(w http.ResponseWriter, req *http.Request) {
+    vars := mux.Vars(req)
     fmt.Println("Json Query handler called!")
+    fmt.Println("Got email " + vars["email"])
 }
 
 func NewAddressFormHandler(w http.ResponseWriter, req *http.Request) {
+    vars := mux.Vars(req)
     fmt.Println("New Address Form Handler called!")
+    fmt.Println("Got uuid " + vars["uuid"])
 }
 
 func NewAddressHandler(w http.ResponseWriter, req *http.Request) {
+    vars := mux.Vars(req)
     fmt.Println("New Address Handler called!")
+    fmt.Println("Got uuid " + vars["uuid"])
 }
 
 func DeleteAddressHandler(w http.ResponseWriter, req *http.Request) {
+    vars := mux.Vars(req)
     fmt.Println("Delete address handler called!")
+    fmt.Println("Got uuid " + vars["uuid"])
 }
