62423df167e59f81b080b7354c95374c9903f371
gerow <gerow.mike@gmail.com>
2013-03-07 21:11:50 -0800

    Added routes for json queries and deletions

--- btcreg.go
+++ btcreg.go
@@ -9,7 +9,9 @@ func Main() {
     r := mux.NewRouter()
     r.HandleFunc("/", HomeHandler).Methods("GET")
     r.HandleFunc("/q/{email}", QueryHandler).Methods("GET")
+    r.HandleFunc("/j/{email}", JsonQueryHandler).Methods("GET")
     r.HandleFunc("/n/{uuid}", NewAddressFormHandler).Methods("GET")
     r.HandleFunc("/n/{uuid}", NewAddressHandler).Methods("POST")
+    r.HandleFunc("/d/{uuid}", DeleteAddressHandler).Methods("DELETE")
     http.ListenAndServe(":8080", r)
 }
--- controllers.go
+++ controllers.go
@@ -2,16 +2,29 @@ package btcreg
 
 import (
     "net/http"
+    "fmt"
 )
 
 func HomeHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("Home handler called!")
 }
 
 func QueryHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("Query handler called!")
+}
+
+func JsonQueryHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("Json Query handler called!")
 }
 
 func NewAddressFormHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("New Address Form Handler called!")
 }
 
 func NewAddressHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("New Address Handler called!")
+}
+
+func DeleteAddressHandler(w http.ResponseWriter, req *http.Request) {
+    fmt.Println("Delete address handler called!")
 }
