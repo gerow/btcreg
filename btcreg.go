8f1aea6d7367c32b34c3b9779de8f46822b76300
gerow <gerow.mike@gmail.com>
2013-03-09 17:39:11 -0800

    Moved most of the stuff in the main into a router module
    This is less to split things up and more to make sure things are well
    named

--- btcreg.go
+++ btcreg.go
@@ -1,24 +1,5 @@
 package btcreg
 
-import (
-    "net/http"
-    "github.com/gorilla/mux"
-)
-
 func Main() {
-    r := mux.NewRouter()
-    r.HandleFunc("/", HomeHandler).Methods("GET")
-    r.HandleFunc("/q/{email}", QueryHandler).Methods("GET")
-    r.HandleFunc("/j/{email}", JsonQueryHandler).Methods("GET")
-    r.HandleFunc("/n/{uuid}", NewAddressFormHandler).Methods("GET")
-    r.HandleFunc("/n/{uuid}", NewAddressHandler).Methods("POST")
-    r.HandleFunc("/d/{uuid}", DeleteAddressHandler).Methods("DELETE")
-    r.PathPrefix("/static/").Handler(http.FileServer(http.Dir(".")))
-
-    // Form methods
-    r.HandleFunc("/add/", AddHandler).Methods("GET")
-    r.HandleFunc("/add/", AddHandlerPost).Methods("POST")
-    r.HandleFunc("/delete/", DeleteHandler).Methods("GET")
-    r.HandleFunc("/about/", AboutHandler).Methods("GET")
-    http.ListenAndServe(":8080", r)
+  RunRouter()
 }
