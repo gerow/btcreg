f08b21e3351b81674ccfaa042e2af2255aaabf3d
gerow <gerow.mike@gmail.com>
2013-03-13 16:29:53 -0700

    Added unimplemented migration tool that should be used to set up db
    Hopefully this should be pretty cool!

--- build_app.sh
+++ build_app.sh
@@ -9,8 +9,11 @@ echo "creating application directory..."
 mkdir app
 echo "done"
 cd app
-echo "executing go build..."
+echo "executing go build on application..."
 go build github.com/gerow/btcreg/btcreg
+echo "done"
+echo "executing go build on migration tool..."
+go build github.com/gerow/btcreg/migrate_db
 echo "done"
 echo "copying necessary files..."
 cp -r ../templates ../static .
