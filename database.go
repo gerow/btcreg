b4eedcf3955255c8d51754d7b32ad369d94ddb46
gerow <gerow.mike@gmail.com>
2013-04-02 21:16:46 -0700

    Made it to where we can hit enter in order to submit queries

--- database.go
+++ database.go
@@ -2,6 +2,7 @@ package btcreg
 
 import (
   "labix.org/v2/mgo"
+  "fmt"
 )
 
 var MongoSession *mgo.Session
@@ -14,10 +15,12 @@ func LoadDatabase() (error) {
 	}
 	MongoSession = s
 	DB = MongoSession.DB("test")
+	fmt.Println("creating indices if they don't already exist...")
 	err = ensureIndices()
 	if err != nil {
 		return err
 	}
+	fmt.Println("done creating indices")
 	return nil
 }
 
@@ -26,7 +29,7 @@ func ensureIndices() (error) {
 		Key: []string{"email"},
 		Unique: true,
 		DropDups: true,
-		Background: true,
+		Background: false,
 		Sparse: true,
 	}
 	err := DB.C("addresses").EnsureIndex(addressIndex)
